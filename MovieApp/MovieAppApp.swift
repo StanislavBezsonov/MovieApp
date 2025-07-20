1ef89debc6886929d7f83070cbcbeb391d837e2a
Stanislav Bezsonov <neosporimiy999@gmail.com>
2025-07-21 01:53:06 +0200

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Stanislav Bezsonov on 21/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
